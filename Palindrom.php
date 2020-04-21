7c92c8627f6b4ebbae4796cdf5ba0d9db285f938
alenagryadun <gryadun@dlit.dp.ua>
2020-04-21 16:04:52 +0300

    finish olindrom

---
+++ Palindrom.php
@@ -0,0 +1,28 @@
+<?php
+class Palindrome
+{
+    public static function isPalindrome($word)
+    {
+        $word_length = strlen($word);
+        if ($word_length < 2 ) return 'Error. It is not a word';
+        $word = strtolower($word);
+        $word_array = str_split($word);
+        if($word_length % 2 == 1) {
+            $middle_index = floor($word_length / 2);
+            array_splice($word_array, $middle_index, 1);
+        }
+        $middle_index = count($word_array) / 2 - 1;
+        $left_path = '';
+        $right_path = '';
+        foreach($word_array as $key => $letter){
+            if($key <= $middle_index) $left_path.= $letter;
+            else $right_path.= $letter;
+        }
+        $right_path = strrev($right_path);
+
+        if($left_path != $right_path) return 'false';
+        return 'true';
+
+    }
+}
+echo Palindrome::isPalindrome('Deleveled'); 
